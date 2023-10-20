6155d0dbf929e0d51fe95b6bbf3a40b0a4e18121
Muhammad-Sowrov <muhammadsowrov53@gmail.com>
2023-10-20 23:18:19 +0600

    private route creation done

--- src/Router/Route.jsx
+++ src/Router/Route.jsx
@@ -8,46 +8,49 @@ import Qna from "../Pages/Qna";
 import AddProduct from "../Pages/AddProduct";
 import Register from "../Pages/Register";
 import Error from "../Pages/Error";
-
+import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <Error></Error>,
+    children: [
+      {
         path: "/",
-        element: <Root></Root>,
-        errorElement: <Error></Error>,
-        children: [
-            {
-                path: "/",
-                element: <Home></Home>,
-                loader: ()=> fetch("/Brand.json")
-            },
-            {
-                path: "/login",
-                element: <Login></Login>
-            },
-            {
-                path: "/register",
-                element: <Register></Register>
-            },
-            {
-                path: "/cart",
-                element: <BookedCart></BookedCart>
-            },
-            {
-                path: "/add-product",
-                element: <AddProduct></AddProduct>
-            },
-            {
-                path: "/about",
-                element: <About></About>
-            },
-            {
-                path: "/faq",
-                element: <Qna></Qna>
-            }
-            
-        ]
-    }
-])
+        element: <Home></Home>,
+        loader: () => fetch("/Brand.json"),
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/cart",
+        element: <BookedCart></BookedCart>,
+      },
+      {
+        path: "/add-product",
+        element: (
+          <PrivateRoute>
+            <AddProduct></AddProduct>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/faq",
+        element: <Qna></Qna>,
+      },
+    ],
+  },
+]);
 
 export default router;
